2b86a204e075acf0e5c23d87f6b1393519c932b5
0x506978656c <66031115+0x506978656c@users.noreply.github.com>
2021-12-29 13:40:26 +0200

    cleaned up using an enum like anyone with a brain would do

--- mod/ChatManager/ChatBuilder.ts
+++ mod/ChatManager/ChatBuilder.ts
@@ -6,36 +6,38 @@
 import {Vec3} from "bdsx/bds/blockpos";
 import {Actor} from "bdsx/bds/actor";
 
+export enum format {
+    /**Colors*/
+    CHAT_BLACK = "§0%s§r",
+    CHAT_DARK_BLUE = "§1%s§r",
+    CHAT_DARK_GREEN = "§2%s§r",
+    CHAT_DARK_AQUA = "§3%s§r",
+    CHAT_DARK_RED = "§4%s§r",
+    CHAT_DARK_PURPLE = "§5%s§r",
+    CHAT_GOLD = "§6%s§r",
+    CHAT_GRAY = "§7%s§r",
+    CHAT_DARK_GRAY = "§8%s§r",
+    CHAT_BLUE = "§9%s§r",
+    CHAT_GREEN = "§a%s§r",
+    CHAT_AQUA = "§b%s§r",
+    CHAT_RED = "§c%s§r",
+    CHAT_LIGHT_PURPLE = "§d%s§r",
+    CHAT_YELLOW = "§e%s§r",
+    CHAT_WHITE = "§f%s§r",
+    CHAT_MINECOIN_GOLD = "§g%s§r",
+    /**Fonts*/
+    CHAT_OBFUSCATED = "§k%s§r",
+    CHAT_BOLD = "§l%s§r",
+    CHAT_STRIKETHROUGH = "§m%s§r",
+    CHAT_UNDERLINE = "§n%s§r",
+    CHAT_ITALIC = "§o%s§r"
+}
+
 export class ChatBuilder {
 
     private mainString: string = "";
-    /**Colors*/
-    public static readonly CHAT_BLACK: "§0%s§r"
-    public static readonly CHAT_DARK_BLUE: "§1%s§r"
-    public static readonly CHAT_DARK_GREEN: "§2%s§r"
-    public static readonly CHAT_DARK_AQUA: "§3%s§r"
-    public static readonly CHAT_DARK_RED: "§4%s§r"
-    public static readonly CHAT_DARK_PURPLE: "§5%s§r"
-    public static readonly CHAT_GOLD: "§6%s§r"
-    public static readonly CHAT_GRAY: "§7%s§r"
-    public static readonly CHAT_DARK_GRAY: "§8%s§r"
-    public static readonly CHAT_BLUE: "§9%s§r"
-    public static readonly CHAT_GREEN: "§a%s§r"
-    public static readonly CHAT_AQUA: "§b%s§r"
-    public static readonly CHAT_RED: "§c%s§r"
-    public static readonly CHAT_LIGHT_PURPLE: "§d%s§r"
-    public static readonly CHAT_YELLOW: "§e%s§r"
-    public static readonly CHAT_WHITE: "§f%s§r"
-    public static readonly CHAT_MINECOIN_GOLD: "§g%s§r"
-    /**Fonts*/
-    public static readonly CHAT_OBFUSCATED: "§k%s§r"
-    public static readonly CHAT_BOLD: "§l%s§r"
-    public static readonly CHAT_STRIKETHROUGH: "§m%s§r"
-    public static readonly CHAT_UNDERLINE: "§n%s§r"
-    public static readonly CHAT_ITALIC: "§o%s§r"
 
-
-    addText(color: ChatBuilder, text: string) {
+    addText(color: format, text: string) {
         this.mainString += color, (text)
     }
 
@@ -43,11 +45,11 @@ export class ChatBuilder {
         this.mainString += text
     }
 
-    addPosition(color: ChatBuilder, pos: Vec3) {
+    addPosition(color: format, pos: Vec3) {
         this.mainString += color, (`[ ${pos.x} ${pos.y} ${pos.z} ]`)
     }
 
-    addActorName(color: ChatBuilder, actor: Actor) {
+    addActorName(color: format, actor: Actor) {
         this.mainString += color, (actor.getName())
     }
 
