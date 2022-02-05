800db288b0a0069fa0f32e7c99b32470e8cec306
0x506978656c <66031115+0x506978656c@users.noreply.github.com>
2022-02-05 12:01:39 +0200

    cleaned up unused hooks

--- mod/Hooks-Tools/SymHook.ts
+++ mod/Hooks-Tools/SymHook.ts
@@ -12,27 +12,6 @@ import {fsutil} from "bdsx/fsutil";
 
 
 const symbols = [
-    "Level::getRuntimeEntity",
-    "Explosion::explode",
-    "MapItemSavedData::_updateTrackedEntityDecoration",
-    "PistonBlock::shouldConnectToRedstone",
-    "PressurePlateBlock::getSignalStrength",
-    "BeaconBlock::canContainLiquid",
-    "GlassBlock::breaksFallingBlocks",
-    "BlockLegacy::breaksFallingBlocks",
-    "PistonBlock::breaksFallingBlocks",
-    "PistonBlockActor::_checkAttachedBlocks",
-    "PistonBlockActor::_handleSlimeConnections",
-    "Dimension::isRedstoneTick",
-    "BlockTickingQueue::tickPendingTicks",
-    "PillagerOutpostFeature::isFeatureChunk",
-    "RepeaterBlock::getTurnOnDelay",
-    "EndCityFeature::isFeatureChunk",
-    "Actor::setNameTag",
-    "AddPlayerPacket::AddPlayerPacket",
-    "PistonBlockActor::ARM_ANIMATION_SPEED",
-    "PistonArmBlock::canBeSilkTouched",
-    "Actor::getBlockPosCurrentlyStandingOn",
     "LevelData::getLevelName",
     "Level::getLevelData"
 ];
--- mod/Hooks-Tools/hookedFunction.ts
+++ mod/Hooks-Tools/hookedFunction.ts
@@ -4,24 +4,17 @@
  *
  */
 
-import {Actor, ActorRuntimeID} from "bdsx/bds/actor";
 import {Level} from "bdsx/bds/level";
 import {bool_t, void_t, uint8_t, CxxString, uint64_as_float_t, int32_t} from "bdsx/nativetype";
-import {Player} from "bdsx/bds/player";
+
 
 import {symHook} from "./SymHook";
-import {Packet} from "bdsx/bds/packet";
-import {SurvivalMode} from "bdsx/bds/gamemode";
-import {BlockPos} from "bdsx/bds/blockpos";
-import {tellAllRaw} from "../ChatManager/MessageManager";
-import {command} from "bdsx/command";
 import {NativeClass, nativeClass} from "bdsx/nativeclass";
-import {serverInstance} from "bdsx/bds/server";
 
 /**
  * hacker.js("symbol:symbool", returnType, options or null "ex) {this:thisType}",  paramtype1, paramtype2);
  */
-export let getRuntimeEntity = symHook.js("Level::getRuntimeEntity", Actor, null, Level, ActorRuntimeID, bool_t);
+//export let getRuntimeEntity = symHook.js("Level::getRuntimeEntity", Actor, null, Level, ActorRuntimeID, bool_t);
 
 @nativeClass(null)
 class LevelData extends NativeClass {
@@ -29,3 +22,4 @@ class LevelData extends NativeClass {
 
 export let getLevelData = symHook.js("Level::getLevelData", LevelData, null, Level);
 export let getLevelName = symHook.js("LevelData::getLevelName", CxxString, null, LevelData);
+
